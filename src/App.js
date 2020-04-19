7e664b687b265fb469315083ad4e0507096d06c1
Gerard Hynes <gkhynes@gmail.com>
2020-04-19 11:52:46 +0100

    Add Balanace, IncomeExpenses and TransactionList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
+import TransactionList from "./components/TransactionList";
 
 import "./App.css";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+        <TransactionList />
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Balance.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Balance = () => {
+  return (
+    <div>
+      <h2>Your Balance</h2>
+      <h3 id="balance">$0.00</h3>
+    </div>
+  );
+};
+
+export default Balance;
